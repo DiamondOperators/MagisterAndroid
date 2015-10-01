9f87162d93df3564bf9b3568bf7eb7d1c3286b17
Tein van der Lugt <teinvdlugt@gmail.com>
2015-10-01 20:07:18 +0200

    Show dialog when click on appointment

--- app/src/main/java/com/diamondoperators/android/magister/MainActivity.java
+++ app/src/main/java/com/diamondoperators/android/magister/MainActivity.java
@@ -1,13 +1,16 @@
 package com.diamondoperators.android.magister;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         AgendaView.OnAppointmentClickListener {
@@ -38,7 +41,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onClickAppointment(Appointment appt) {
-        Snackbar.make(drawerLayout, "Clicked on appointment " + appt.getSubject(), Snackbar.LENGTH_LONG).show();
+        DateFormat format = DateFormat.getTimeInstance(DateFormat.SHORT);
+        String startTime = format.format(new Date(appt.getStartTime() * 1000));
+        String endTime = format.format(new Date(appt.getEndTime() * 1000));
+
+        String title = appt.getSubject() != null ? appt.getSubject() : "Onbekend vak";
+        StringBuilder msg = new StringBuilder();
+        if (appt.getTeacher() != null) msg.append("Leraar: ").append(appt.getTeacher());
+        if (appt.getLocation() != null) msg.append("\nLocatie: ").append(appt.getLocation());
+        if (appt.getGroup() != null) msg.append("\nGroep: ").append(appt.getGroup());
+        if (appt.getStartTime() != 0) msg.append("\nStarttijd: ").append(startTime);
+        if (appt.getEndTime() != 0) msg.append("\nEindtijd: ").append(endTime);
+        if (appt.getDescription() != null)
+            msg.append("\nBeschrijving: ").append(appt.getDescription());
+
+        new AlertDialog.Builder(this)
+                .setTitle(title).setMessage(msg)
+                .setPositiveButton(android.R.string.ok, null)
+                .create().show();
     }
 
     private void setupDrawerToggle() {
